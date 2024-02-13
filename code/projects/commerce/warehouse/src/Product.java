8939d1556b9161a6bba2fd0d31b300c04fff4bb0
atka03 <adi.athiththiyan.kailainathan@gmail.com>
2024-02-13 15:20:16 +0100

    Legget til getter metoder for alle feltene i Product klassen

--- code/projects/commerce/warehouse/src/Product.java
+++ code/projects/commerce/warehouse/src/Product.java
@@ -2,6 +2,15 @@ package projects.commerce.warehouse.src;
 
 public class Product
 {
+    //# Fields
     private String name;
     private double price;
+
+    //# Getters
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
 }

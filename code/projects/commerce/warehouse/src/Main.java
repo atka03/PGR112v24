a4fcd6e461b51ce91845f40e8f182c8ddc2a952c
atka03 <adi.athiththiyan.kailainathan@gmail.com>
2024-02-13 15:27:37 +0100

    Lagget et Warehaouse warehouse i Main klassen og laget tre Produckt objekter

--- code/projects/commerce/warehouse/src/Main.java
+++ code/projects/commerce/warehouse/src/Main.java
@@ -5,7 +5,14 @@ public class Main
     public static void main(String[] args) {
         Store store = new Store();
 
+        Warehouse warehouse = new Warehouse(store);
+
         //# Opprett mange forskjellige produkter
+        Product product0 = new Product("Vaskemaskin", 1000.99);
+        Product product1 = new Product("PlayStation 5", 10000);
+        Product product2 = new Product("Nintendo Switch", 5000);
+
+
         // og legg de til i lageret
 
         //# Flytt varer fra lageret til butikken
